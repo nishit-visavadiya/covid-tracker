a5a967e1b2b0a64ee706490e211b0c7bd9efbea4
nishit-visavadiya <64531612+nishit-visavadiya@users.noreply.github.com>
2021-07-18 15:41:05 +0530

    covid tracker

---
+++ app/src/main/java/com/thenextlevel/covidtracker/CountryActivity.java
@@ -0,0 +1,114 @@
+package com.thenextlevel.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.thenextlevel.covidtracker.api.ApiUtilities;
+import com.thenextlevel.covidtracker.api.CountryData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CountryActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<CountryData> list;
+    private ProgressDialog dialog;
+    private EditText searchBar;
+
+    //private androidx.appcompat.widget.SearchView searchBar;
+
+    private CountryAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+
+        //searchBar = findViewById(R.id.searchBar);
+        recyclerView = findViewById(R.id.countries);
+        list = new ArrayList<>();
+        searchBar = findViewById(R.id.searchBar);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new CountryAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        ApiUtilities.getApiInterface().getCountryData().enqueue(new Callback<List<CountryData>>() {
+            @Override
+            public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
+                list.addAll(response.body());
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<CountryData>> call, Throwable t) {
+                Toast.makeText(CountryActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
+
+        /*searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return true;
+            }
+        });*/
+
+    }
+
+    private void filter(String text) {
+        List<CountryData> filterList = new ArrayList<>();
+
+        for (CountryData items : list) {
+            if (items.getCountry().toLowerCase().contains(text.toLowerCase())) {
+                filterList.add(items);
+            }
+        }
+        adapter.filterList(filterList);
+    }
+}
